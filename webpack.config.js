cd669b7943c38d98674b63f048f55746c5214a35
Nikita <nikita.romanchuk.97@yandex.ru>
2019-12-16 14:37:40 +0300

    fixed webpack config; added custom-media & generic layouts; fixed grid column variables;

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 require('dotenv').config();
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-// Minifying
+// Minimizing bundle
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -25,7 +25,14 @@ module.exports = {
   optimization: {
     minimize: !isDevMode,
     splitChunks: {
-      chunks: "all"
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        }
+      },
     },
     minimizer: [
       new UglifyJsPlugin({
@@ -36,9 +43,9 @@ module.exports = {
   },
 
   resolve: {
-		extensions: ['.js', '.css'],
+    extensions: ['.js', '.css'],
   },
-  
+
   module: {
     rules: [
       {
@@ -52,13 +59,18 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               plugins: (loader) => [
-                postCssPresetEnv(),
                 require('autoprefixer')({
-                  flexbox : true,
+                  flexbox: true,
                 }),
                 require('postcss-import')({ root: loader.resourcePath }),
-                require('postcss-preset-env')(),
                 require('cssnano')(),
+                postCssPresetEnv({
+                  features: {
+                    'custom-properties': true,
+                    'custom-media-queries': true
+                  },
+                  importFrom: path.join(__dirname, 'src/styles/customMedia.css')
+                }),
               ]
             }
           },
@@ -66,25 +78,37 @@ module.exports = {
       },
 
       {
-				test: /\.(svg|png|jpg|woff|woff2|eot|ttf|otf)$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-				},
-			},
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets',
+        }
+      },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ 
-      template: './src/index.html' 
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
     }),
-
     new CleanWebpackPlugin(),
-    
+    new CopyWebpackPlugin([
+      {
+        from: './src/assets',
+        to: 'assets/'
+      }
+    ]),
+
     new MiniCssExtractPlugin({
       filename: 'style.css',
       chunkFilename: '[id].css',
       ignoreOrder: false
-    }),
+    })
   ]
 };
